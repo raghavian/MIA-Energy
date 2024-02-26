57ec9da12193b7d29ee8e7d6ecbb689c0bc45097
Raghav <raghav@di.ku.dk>
2024-02-26 17:40:32 +0100

    Option for training with smaller datasets

--- datasets.py
+++ datasets.py
@@ -13,13 +13,21 @@ from numpy import random
 from torch.nn.functional import avg_pool2d
 
 class DermaMNIST(Dataset):
-     def __init__(self, data_loc = '/home/image/raghav/datasets/medMNIST/DermaMNIST/',split='train', 
+     def __init__(self, data_loc = '/home/image/raghav/datasets/medMNIST/DermaMNIST/',\
+             split='train',\
+             num_data = -1,\
              transform=None):
           super().__init__()
 
 #          pdb.set_trace()
           self.transform = transform
           self.data, self.targets = torch.load(data_loc+split+'.pt')
+          if num_data != -1:
+              print('Using subset of data')
+              idx = torch.randperm(len(self.targets))
+
+              self.data, self.targets = self.data[idx[:num_data]], self.targets[idx[:num_data]] 
+
 
      def __len__(self):
           return len(self.targets)
